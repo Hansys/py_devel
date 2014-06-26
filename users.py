7bc317e7b7868701743746a27d80cddbf37e7f27
Hansys <hans.balis@gmail.com>
2014-06-26 15:25:37 +0200

    Create users.py

---
+++ users.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from xmlrpclib import ServerProxy
+
+SERVER = 'http://localhost:8069'
+DATABASE = 'demo'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+server = ServerProxy('http://localhost:8069/xmlrpc/common')
+user_id = server.login(DATABASE, USERNAME, PASSWORD)
+
+server = ServerProxy('http://localhost:8069/xmlrpc/object')
+user_ids = server.execute(
+    DATABASE, user_id, PASSWORD,
+    'res.users', 'search', []
+)
+
+users = server.execute(
+    DATABASE, user_id, PASSWORD,
+    'res.users', 'read', user_ids, []
+)
+
+for user in users:
+    print(user['id'], user['name'])
